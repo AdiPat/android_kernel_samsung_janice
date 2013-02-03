688dbd01cb0f98d8d53e7a11074678f153b85a4e
AdiPat <adithemagnificent@gmail.com>
2013-02-03 21:43:38 +0530

    sound: ab8500_sound_control: Improvements to the driver (v2)
    -> Add code for Jack Detection, when the headset is detected,
       the sound boost option in our Kernel tweaker app will be
       available.
    -> Power on the soc when we enable sound control
    -> Fix balancing issues
    
    Thanks to @cocafe for some ideas and testing. :)
    
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

--- sound/soc/codecs/ab8500_sound_control.c
+++ sound/soc/codecs/ab8500_sound_control.c
@@ -20,12 +20,16 @@
 #include <sound/ab8500_sound_control.h>
 
 #define DRIVER_NAME                        "AB8500 Sound boost control"
-#define SOUND_BOOST_CONTROL_VERSION             1
-/* Headset Audio Configs */ 
-#define REG_ANAGAIN3_HSLGAIN_BOOST		3
-#define REG_ANAGAIN3_HSRGAIN_BOOST		3
+#define SOUND_BOOST_CONTROL_VERSION             2
+/* Boost Audio */ 
+#define REG_ANAGAIN3_BOOST                      0
+#define REG_ANAGAIN3_DEFAULT                    68
 /* Toggle */ 
 static int sound_boost_status = 0;
+extern int ab850x_audio_power_control(bool power_on);
+
+/* Check whether headset is plugged in */ 
+extern int jack_is_detected;
 
 static struct snd_soc_codec *sound_boost_codec;
 
@@ -36,6 +40,20 @@ int sound_boost_register(struct snd_soc_codec *codec)
 	if(sound_boost_codec == NULL)
 		printk(KERN_ERR "[ab8500-Sound-Control] Failed to register ab8500 codec!\n");
 	return 0;
+}
+
+/* Check status of headset jack */
+
+int get_jack_status(void)
+{
+	return jack_is_detected;
+}
+
+static ssize_t jack_status_read(struct kobject *kobj, struct kobject_attribute *attr,
+				char *buf)
+{
+	int status = get_jack_status();
+	return sprintf(buf, "%d\n", status);
 }
 
 static ssize_t sound_boost_control_show(struct kobject *kobj, struct kobj_attribute *attr,
@@ -51,14 +69,14 @@ static ssize_t sound_boost_control_store(struct kobject *kobj, struct kobj_attri
 	if (sound_boost_status == 0)
 	{
 		printk(KERN_DEBUG "[ab8500-Sound-Control] Sound boost disabled\n");
-		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_HSRGAIN);
-		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_HSLGAIN);
+		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_DEFAULT);
 	}
 	if (sound_boost_status == 1)
 	{
+		/* Power on the ab8500 SoC */ 
+		ab850x_audio_power_control(true);		
 		printk(KERN_DEBUG "[ab8500-Sound-Control] Sound boost enabled\n");
-		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_HSRGAIN_BOOST);
-		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_HSLGAIN_BOOST);
+		snd_soc_write(sound_boost_codec, REG_ANAGAIN3, REG_ANAGAIN3_BOOST);
 	}
 	else
 	{
@@ -74,10 +92,12 @@ static ssize_t sound_boost_control_version_show(struct kobject *kobj, struct kob
 
 static struct kobj_attribute sound_boost_control_interface   = __ATTR(sound_boost, 0666, sound_boost_control_show, sound_boost_control_store);
 static struct kobj_attribute sound_boost_control_version     = __ATTR(version, 0666, sound_boost_control_version_show, NULL);
+static struct kobj_attribute jack_status                     = __ATTR(jack_status, 0666, jack_status_read, NULL);
 
 static struct attribute *snd_boost_control_attrs[] = {
 	&sound_boost_control_interface.attr,
 	&sound_boost_control_version.attr, 
+	&jack_status.attr, 
 	NULL,
 };
 
